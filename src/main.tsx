1daed7e990bf81c109fb2eb2bbc85fc022814314
davidemarcoli <davide@marcoli.ch>
2023-02-21 16:11:42 +0100

    Add ESLint and Prettier

--- src/main.tsx
+++ src/main.tsx
@@ -6,5 +6,5 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <StyledApp />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
