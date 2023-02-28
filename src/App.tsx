5ecdbe700ec410d0b06e87290c96f20c2ed406a2
Akbar-Noser <Oliver.Pettersson@noseryoung.ch>
2023-02-28 13:32:37 +0100

    corrected init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import './App.css'
-import React from 'react'
-import { Box, createTheme, IconButton, ThemeProvider, useTheme } from '@mui/material'
-import { BsFillMoonStarsFill, BsSun } from 'react-icons/all'
 
 function App() {
   return (
