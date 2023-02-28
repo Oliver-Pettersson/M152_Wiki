acded335b280708b85b16b20055c5b65b564b132
davidemarcoli <davide@marcoli.ch>
2023-02-28 15:08:54 +0100

    Add software evaluation page

--- src/StyledApp.tsx
+++ src/StyledApp.tsx
@@ -1,60 +1,69 @@
 import './App.css'
 import React from 'react'
-import { Box, Container, createTheme, IconButton, ThemeProvider, useTheme } from '@mui/material'
-import { BsFillMoonStarsFill, BsSun } from 'react-icons/all'
+import {Box, createTheme, ThemeProvider} from '@mui/material'
 import App from './App'
 
 export const ColorModeContext = React.createContext({
-  toggleColorMode: () => {}
+    toggleColorMode: () => {}
 })
 
 function StyledApp() {
-  return (
-    <Box
-      sx={{
-        width: '100vw',
-        height: '100vh',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'column',
-        bgcolor: 'background.default',
-        color: 'text.primary'
-      }}
-    >
-      <App />
-    </Box>
-  )
+    return (
+        <Box
+            sx={{
+                width: '100vw',
+                height: '100vh',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                flexDirection: 'column',
+                bgcolor: 'background.default',
+                color: 'text.primary'
+            }}
+        >
+            <App/>
+        </Box>
+    )
 }
 
 export default function ToggleColorMode() {
-  const [mode, setMode] = React.useState<'light' | 'dark'>('light')
-  const colorMode = React.useMemo(
-    () => ({
-      toggleColorMode: () => {
-        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'))
-      }
-    }),
-    []
-  )
+    const [mode, setMode] = React.useState<'light' | 'dark'>('light')
+    const colorMode = React.useMemo(
+        () => ({
+            toggleColorMode: () => {
+                setMode((prevMode) => {
+                    const mode = prevMode === 'light' ? 'dark' : 'light'
+                    console.log('toggleColorMode', mode)
+                    if (mode === 'dark') {
+                        document.documentElement.classList.add('dark')
+                    } else {
+                        document.documentElement.classList.remove('dark')
+                    }
+                    return mode
+                })
+                // setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'))
+            }
+        }),
+        []
+    )
 
-  const theme = React.useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode
-        }
-      }),
-    [mode]
-  )
+    const theme = React.useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    mode
+                }
+            }),
+        [mode]
+    )
 
-  return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <StyledApp />
-      </ThemeProvider>
-    </ColorModeContext.Provider>
-  )
+    return (
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <StyledApp/>
+            </ThemeProvider>
+        </ColorModeContext.Provider>
+    )
 }
 
 // export default App
---
+++ src/components/pages/SoftwareLibraryPage/SoftwareLibraryPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Title} from "@mui/icons-material";
+
+export default function SoftwareLibraryPage() {
+    return (
+        <Title>Evaluation</Title>
+    );
+}
