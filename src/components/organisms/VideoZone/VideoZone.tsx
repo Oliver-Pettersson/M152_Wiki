1c2f66d74aa41438dafc963eeeab7749b7338955
Akbar-Noser <Oliver.Pettersson@noseryoung.ch>
2023-03-14 16:29:10 +0100

    fixed 400 videos

--- src/components/organisms/VideoZone/VideoZone.tsx
+++ src/components/organisms/VideoZone/VideoZone.tsx
@@ -3,43 +3,33 @@ import React from "react";
 import VideoCard from "../../molecules/VideoCard/VideoCard";
 
 const VideoZone = () => {
-  const width = useWindowWidth()
-
-  const videoList = []
-    for (let i = 0; i < 400; i++) {
-        videoList.push(
-            <VideoCard
-            img="https://www.youtube.com/embed/rf8YebDBcbY"
-            desc={"OKIDOKI " + i}
-            key={i}
-            />
-        )
-    }
-
+  const width = useWindowWidth();
   return (
     <>
-      <div className={width > 600 ? "grid gap-4 grid-cols-2" : "grid gap-4 grid-cols-1"}>
-        {/*<VideoCard*/}
-        {/*  img="https://www.youtube.com/embed/rf8YebDBcbY"*/}
-        {/*  desc="OKIDOKI"*/}
-        {/*/>*/}
-        {/*<VideoCard*/}
-        {/*  img="https://www.youtube.com/embed/rf8YebDBcbY"*/}
-        {/*  desc="OKIDOKI"*/}
-        {/*/>*/}
-        {/*<VideoCard*/}
-        {/*  img="https://www.youtube.com/embed/rf8YebDBcbY"*/}
-        {/*  desc="OKIDOKI"*/}
-        {/*/>*/}
-        {/*<VideoCard*/}
-        {/*  img="https://www.youtube.com/embed/rf8YebDBcbY"*/}
-        {/*  desc="OKIDOKI"*/}
-        {/*/>*/}
-
-        {videoList}
+      <div
+        className={
+          width > 600 ? "grid gap-4 grid-cols-2" : "grid gap-4 grid-cols-1"
+        }
+      >
+        <VideoCard
+          img="https://www.youtube.com/embed/rf8YebDBcbY"
+          desc={"OKIDOKI"}
+        />
+        <VideoCard
+          img="https://www.youtube.com/embed/rf8YebDBcbY"
+          desc={"OKIDOKI"}
+        />
+        <VideoCard
+          img="https://www.youtube.com/embed/rf8YebDBcbY"
+          desc={"OKIDOKI"}
+        />
+        <VideoCard
+          img="https://www.youtube.com/embed/rf8YebDBcbY"
+          desc={"OKIDOKI"}
+        />
       </div>
     </>
   );
-}
+};
 
-export default VideoZone
+export default VideoZone;
