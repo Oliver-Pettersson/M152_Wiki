0ccfa39677ab942ad4f4b35b475493ec18048463
Sashimi4 <sascha.buthelezi@noseryoung.ch>
2023-04-04 16:32:20 +0200

    stats card zoom in animation.

--- src/components/molecules/StatsCard/StatsCard.tsx
+++ src/components/molecules/StatsCard/StatsCard.tsx
@@ -2,25 +2,25 @@ import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
 import GroupIcon from "@mui/icons-material/Group";
 import PsychologyIcon from "@mui/icons-material/Psychology";
 import { Typography } from "@mui/material";
+import "./StatsCard.css"
 
 function StatsCard(props: any) {
   return (
     <>
       <div className="grid lg:grid-cols-3 sm:grid-cols-3 xs:grid-cols-3 gap-16 h-fit pt-16 pb-16 text-center">
-        
-        <div className="items-center flex flex-col border-t-4 border-[#62DB44] px-3 pt-4 md:w-40 lg:w-60">
+        <div className="stat-card items-center flex flex-col border-t-4 border-[#62DB44] px-3 pt-4 md:w-40 lg:w-60">
           <Typography>
             Customers inspired by us
           </Typography>
           <Typography sx={{fontSize: "2.5rem" }}>55K+</Typography>
         </div>
-        <div className="items-center flex flex-col border-t-4 border-[#62DB44] px-3 pt-4 md:w-40 lg:w-60">
+        <div className="stat-card items-center flex flex-col border-t-4 border-[#62DB44] px-3 pt-4 md:w-40 lg:w-60">
           <Typography>
             Many years of success
           </Typography>
           <Typography sx={{fontSize: "2.5rem" }}>12+</Typography>
         </div>
-        <div className="items-center flex flex-col border-t-4 border-[#62DB44] px-3 pt-4  md:w-40 lg:w-60">
+        <div className="stat-card items-center flex flex-col border-t-4 border-[#62DB44] px-3 pt-4  md:w-40 lg:w-60">
           <Typography>
             Experts for your experience
           </Typography>
