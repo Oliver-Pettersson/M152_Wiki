b0a1c34831702673cc1f91d7343b85f76b0f4740
Sashimi4 <sascha.buthelezi@noseryoung.ch>
2023-04-11 14:22:00 +0200

    illustrat v.1

--- src/components/pages/VideoPage/VideoPage.tsx
+++ src/components/pages/VideoPage/VideoPage.tsx
@@ -3,11 +3,11 @@ import VideoZone from '../../organisms/VideoZone/VideoZone';
 
 
 export default function VideoPage() {
-
-    return (
-        <>
-            <Carousel/>
-            <VideoZone/>
-        </>
-    )
+  return (
+    <>
+      <Carousel />
+      <VideoZone/>
+      <img src="public\image\undraw_landscape_photographer_5nvi.svg"/>
+    </>
+  )
 }
