c79ab2333e19a051c14676bab64d866aea94384a
davidemarcoli <davide@marcoli.ch>
2023-05-07 19:03:44 +0200

    Replace sharp s with double s

--- src/components/organisms/VideoDetail/docker/DockerVideo.tsx
+++ src/components/organisms/VideoDetail/docker/DockerVideo.tsx
@@ -9,12 +9,12 @@ import AnimatedPage from "../../../pages/AnimatedPage";
 function DockerVideo() {
 
     const storyboardItems = [
-        "Hier wird der Zuschauer begrüßt und es wird ihm ein Überblick über das Video gegeben. Außerdem wird ihm erklärt, was Docker ist und was es kann. Es wird jedoch nicht zu sehr ins Detail gegangen, da dies den Rahmen der Einleitung sprengen würde.",
-        "Bei diesem Punkt wird dem Zuschauer erklärt was Docker-Container sind und wie sie funktionieren. Es wird auch auf die Unterschiede zwischen Containern und VMs eingegangen. Außerdem wird dem Zuschauer erklärt, wie Docker-Container aufgebaut sind und wie sie funktionieren. Ebenfalls wird darauf eingegangen, wie lightweight Docker-Container sind.",
-        "Hier wird dem Zuschauer die Isolation der Docker-Container erklärt. Es wird gesagt, dass sie losgelöst von der zugrundeliegenden Infrastruktur sind und dass sie nur die Ressourcen nutzen, die sie benötigen. Außerdem wird dem Zuschauer erklärt, dass Docker-Container nicht nur auf einem System laufen können, sondern auch auf mehreren Systemen.",
+        "Hier wird der Zuschauer begrüsst und es wird ihm ein Überblick über das Video gegeben. Ausserdem wird ihm erklärt, was Docker ist und was es kann. Es wird jedoch nicht zu sehr ins Detail gegangen, da dies den Rahmen der Einleitung sprengen würde.",
+        "Bei diesem Punkt wird dem Zuschauer erklärt was Docker-Container sind und wie sie funktionieren. Es wird auch auf die Unterschiede zwischen Containern und VMs eingegangen. Ausserdem wird dem Zuschauer erklärt, wie Docker-Container aufgebaut sind und wie sie funktionieren. Ebenfalls wird darauf eingegangen, wie lightweight Docker-Container sind.",
+        "Hier wird dem Zuschauer die Isolation der Docker-Container erklärt. Es wird gesagt, dass sie losgelöst von der zugrundeliegenden Infrastruktur sind und dass sie nur die Ressourcen nutzen, die sie benötigen. Ausserdem wird dem Zuschauer erklärt, dass Docker-Container nicht nur auf einem System laufen können, sondern auch auf mehreren Systemen.",
         "In diesem Abschnitt wird erklärt, auf welche Arten Docker gebraucht werden kann. Entweder lokal oder über den Docker Playground. Die Benutzung vom Docker Playground wird dabei Schritt für Schritt erklärt.",
-        "Hier wird auf den wichtigsten Befehl in Docker eingegangen: 'docker run' und was er bewirkt. Außerdem wird der Command dem Zuschauer anhand des hello-world Containers erklärt. Auch das Pullen von Images wird hier erwähnt.",
-        "Dies ist der Schluss des Videos. Hier wird dem Zuschauer nochmal ein Überblick über das Video gegeben und es wird ihm erklärt, dass er nun in der Lage ist, Docker zu benutzen. Außerdem werden ihm noch viele weitere Ressourcen gegeben, die ihm helfen können, Docker zu lernen.",
+        "Hier wird auf den wichtigsten Befehl in Docker eingegangen: 'docker run' und was er bewirkt. Ausserdem wird der Command dem Zuschauer anhand des hello-world Containers erklärt. Auch das Pullen von Images wird hier erwähnt.",
+        "Dies ist der Schluss des Videos. Hier wird dem Zuschauer nochmal ein Überblick über das Video gegeben und es wird ihm erklärt, dass er nun in der Lage ist, Docker zu benutzen. Ausserdem werden ihm noch viele weitere Ressourcen gegeben, die ihm helfen können, Docker zu lernen.",
     ]
 
     return (
@@ -50,7 +50,7 @@ function DockerVideo() {
                             allowFullScreen></iframe>
                     <Fade bottom>
                         <Typography className="video-thumbnail-description row-span-2 col-span-1">
-                            Willkommen zu diesem Einführungsvideo über Docker! In diesem Video werden wir die Grundlagen von Docker erläutern und Ihnen eine Einführung in diese äußerst leistungsfähige Container-Plattform geben.
+                            Willkommen zu diesem Einführungsvideo über Docker! In diesem Video werden wir die Grundlagen von Docker erläutern und Ihnen eine Einführung in diese äusserst leistungsfähige Container-Plattform geben.
 
                             Docker ist eine Anwendung, die es Ihnen ermöglicht, Anwendungen in Containern zu isolieren und auszuführen. Container sind wie virtuelle Maschinen, aber sie sind viel leichter und schneller als traditionelle VMs. Docker ermöglicht es Entwicklern und IT-Profis, Anwendungen schnell bereitzustellen, zu skalieren und zu verwalten, ohne sich Gedanken über die zugrunde liegende Infrastruktur machen zu müssen.
                         </Typography>
